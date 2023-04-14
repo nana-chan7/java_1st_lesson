9f50b6077e38d50ee9008938175a67ec09521430
{marimo_7} <{220098tt@yse-c.net}>
2023-04-14 14:10:55 +0900

    6-period

--- src/item/Drink.java
+++ src/item/Drink.java
@@ -6,6 +6,34 @@ package item;
 // ↑ 消去するとエラー
 // Ctrl + . (クイックフィックス)でヒントをくれる → タイピングしなくてもある程度修正できる
 public class Drink {
-    
+    // Java は静的型付け (python は動的型付け) 静的のほうがバグが少ないかも？
+    public static void main(String[] args) {
+        // データ型 変数名;
+        // 名称 (文字列型)
+        String name;
+        // 価格 (整数型)
+        int price;
+        // 個数 (整数型)
+        int amount;
+        // セール品か？ (boolean型)
+        boolean isSale;
+
+        name = "コーヒー";
+        price = 150;
+        amount = 3;
+        isSale = true;  // ← すべて小文字(python と違う)
+
+        System.out.println(name);
+        System.out.println(price);
+        System.out.println(amount);
+        System.out.println(isSale);
+
+        amount = 2;
+
+        // ↓ 宣言した型と違うのでエラーになる
+        // amount = "2";
+        // isSale = 1;
+        // name = 1111;
+    }
 }
 
