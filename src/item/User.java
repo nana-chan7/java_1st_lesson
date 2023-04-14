c844de0fb85f3a2f72bef7ff609339883fe293ee
{marimo_7} <{220098tt@yse-c.net}>
2023-04-14 14:23:01 +0900

    end2

--- src/item/User.java
+++ src/item/User.java
@@ -2,11 +2,15 @@ package item;
 
 public class User {
     public static void main(String[] args) {
-        
-        String name;        // 氏名
-        int no;             // 会員番号
-        String email;       // Email
-        boolean isActive;   // 有効
+
+        // 氏名
+        String name;
+        // 会員番号
+        int no;
+        // Email
+        String email;
+        // 有効
+        boolean isActive;
+
     }
-    
 }
