841e0353e9885f8c28c7abc4779cd0fd25c128c0
{marimo_7} <{220098tt@yse-c.net}>
2023-04-14 14:20:38 +0900

    end

---
+++ src/item/User.java
@@ -0,0 +1,12 @@
+package item;
+
+public class User {
+    public static void main(String[] args) {
+        
+        String name;        // 氏名
+        int no;             // 会員番号
+        String email;       // Email
+        boolean isActive;   // 有効
+    }
+    
+}
