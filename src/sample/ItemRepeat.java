59a2e9861f90fb13c075e90b8529702716f77b6e
{marimo_7} <{220098tt@yse-c.net}>
2023-04-28 13:45:53 +0900

    for文

---
+++ src/sample/ItemRepeat.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class ItemRepeat {
+    
+    public static void main(String[] arg) {
+
+        // for (fori を選択)
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
