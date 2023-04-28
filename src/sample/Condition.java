91b154197aec08198a3c2456019f11715fed74cc
{marimo_7} <{220098tt@yse-c.net}>
2023-04-28 09:15:34 +0900

    if文

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -4,7 +4,7 @@ package sample;
 public class Condition {
     public static void main(String[] args) {
         // 価格
-        int price = 200;
+        int price = -200;
         // 所持金
         int money = 100;
         // メッセージ
@@ -17,9 +17,19 @@ public class Condition {
         //     message = "所持金が足りません。";
         // }
 
-        if (money >= price) {
+        // if (money >= price) {
+        //     message = "お買い上げありがとうございます。";
+        // } else { 
+        //     message = "所持金が足りません。";
+        // }
+
+        if (price <= 0){
+            message = "価格が正しくありません。";
+        }
+        else if (money >= price){
             message = "お買い上げありがとうございます。";
-        } else { 
+        }
+        else {
             message = "所持金が足りません。";
         }
 
