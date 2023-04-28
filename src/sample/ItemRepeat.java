d9010cc5aed175fa304898e2991c18df0c4a2f72
{marimo_7} <{220098tt@yse-c.net}>
2023-04-28 14:06:19 +0900

    for文

--- src/sample/ItemRepeat.java
+++ src/sample/ItemRepeat.java
@@ -1,12 +1,20 @@
 package sample;
-
+// 繰り返し処理
 public class ItemRepeat {
     
     public static void main(String[] arg) {
 
-        // for (fori を選択)
+        // for文 (fori を選択)
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+        }
+
+        // forのネスト
+        for (int i = 1; i < 10; i++) {
+            for (int j = 1; j < 10; j++) {
+                String plot = "(" + i + "," + j + ")";
+                System.out.println(plot);
+            }
         }
     }
 }
