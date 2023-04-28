fb75e064619617840a01a6b7755652568a6e34e2
{marimo_7} <{220098tt@yse-c.net}>
2023-04-28 10:27:18 +0900

    calenderクラス

--- src/sample/Condition.java
+++ src/sample/Condition.java
@@ -47,22 +47,29 @@ public class Condition {
 
         // switch 文 
         // 予測の右側にある Switch Statement を選択  
-        String dayString = "金";
+
+        // String dayString = "金";
         // カレンダー機能
         Calendar cal = Calendar.getInstance();
         int weekIndex = cal.get(Calendar.DAY_OF_WEEK);
         System.out.println(weekIndex);
         
         String garbage = "";
-        switch (dayString) {
+        // switch (dayString) {
+        switch (weekIndex) {
             // 同じ処理をしたい case は複数でもよい
-            case "月":
-            case "金":
+            // case "月":
+            // case "金":
+            // ↓ MONDAYのところ を Ctrl + クリックで calenderクラスを表示
+            case Calendar.MONDAY:
+            case Calendar.FRIDAY:
+
                 garbage = "燃えるゴミ";
                 // ↓ break がないと処理がおかしくなる
                 break;
 
-            case "水":
+            // case "水":
+            case Calendar.WEDNESDAY:
                 garbage = "燃えないゴミ";
                 break;
 
@@ -70,6 +77,7 @@ public class Condition {
                 garbage = "回収なし";
                 break;
         }
+
         System.out.println(garbage);
 
     }
