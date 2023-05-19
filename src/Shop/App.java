dcfb7b32f0573abf01c2387fc9254841ae3e59fe
{marimo_7} <{220098tt@yse-c.net}>
2023-05-19 14:40:46 +0900

    2023/05/18

---
+++ src/Shop/App.java
@@ -0,0 +1,10 @@
+package Shop;
+
+public class App {
+    public static void main(String[] args) {
+        Drink coffee = new Drink("コーヒー", 350, 100);
+        Drink tea = new Drink("紅茶", 400, 10);
+        Drink roasted_green_tea = new Drink("ほうじ茶", 300, 50);
+
+    }
+}
---
+++ src/Shop/Drink.java
@@ -0,0 +1,13 @@
+package Shop;
+
+public class Drink {
+    public String name;
+    public int price;
+    public int stock;
+
+    public Drink(String name, int price, int stock) {
+        this.name = name;
+        this.price = price;
+        this.stock = stock;
+    }
+}
