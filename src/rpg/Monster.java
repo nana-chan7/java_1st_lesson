7011d177f4fe88686d10626314330172a8422c6a
{marimo_7} <{220098tt@yse-c.net}>
2023-05-19 10:11:34 +0900

    monster class

--- src/rpg/Monster.java
+++ src/rpg/Monster.java
@@ -1,5 +1,20 @@
 package rpg;
 
 public class Monster {
-    
+    // field (property)
+    public String name;
+    public String job;
+    public int hp;
+    public int mp;
+    public int lrevel;
+    public int exp;
+    public int attackPower;
+    public int defencePower;
+
+    public void attack(Character character) {
+        int damage = this.attackPower - character.defencePower;
+        // ダメージが 0より大きい場合は、相手のHPを減らす
+        if(damage > 0) character.hp -= damage;
+    }
+
 }
