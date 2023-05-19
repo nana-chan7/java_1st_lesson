c70871d2e36c10fd20504acb0e4252f69c94d730
{marimo_7} <{220098tt@yse-c.net}>
2023-05-19 11:29:50 +0900

    warriorの攻撃をランダムにクリティカルヒットさせる

--- src/rpg/Warrior.java
+++ src/rpg/Warrior.java
@@ -1,5 +1,7 @@
 package rpg;
 
+import java.util.Random;
+
 public class Warrior extends Character{
 
     public Warrior(String name) {
@@ -15,6 +17,19 @@ public class Warrior extends Character{
     @Override
     public void attack(Monster monster) {
         System.out.println("オーバーライド");
+        // 初期の攻撃力を tmp(一時保存)する
+        int attackPower = this.attackPower;
+        // 1/5 の確率でクリティカルヒットする
+        Random rand = new Random();
+        int index = rand.nextInt(5);
+        if(index == 0){
+            // 攻撃力を 2倍する
+            System.out.println("Critical Hit!!");
+            this.attackPower *=2;
+        }
+        // 攻撃
         super.attack(monster);
+        // 攻撃力を元に戻す
+        this.attackPower = attackPower;
     }
 }
