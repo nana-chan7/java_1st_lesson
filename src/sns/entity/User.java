2513866f235cdf53488c12c63524fca5b3a9e482
{marimo_7} <{220098tt@yse-c.net}>
2023-05-26 12:16:56 +0900

    update

--- src/sns/entity/User.java
+++ src/sns/entity/User.java
@@ -3,6 +3,19 @@ package sns.entity;
 
 public class User {
     
-    private String email;
+    // ↓ みんなに見える
+    // public String email;
 
+    // ↓ ほかのファイルの email がエラーになる
+    private String email;
+    private String name;
+    private String password;
+
+    // ↓ 違うパッケージだと認識できなくなる
+    // ↓ 自分の家族だけ知っている感じ
+    // protected String email;
+
+    // ↓ 同じパッケージの中なら認識できる
+    // String email;
+    
 }
