c466b85830422d392141fb9e00540d1efb31463b
{marimo_7} <{220098tt@yse-c.net}>
2023-05-26 13:32:47 +0900

    update

--- src/sns/entity/App.java
+++ src/sns/entity/App.java
@@ -4,8 +4,14 @@ package sns.entity;
 public class App {
     public static void main(String[] args) {
         User user = new User();
+
         // user.email = "yse@test.com";
         user.setEmail("yse@test.com");
+        user.setName("東京 太郎");
+        user.setPassword("xxxxxxx");
 
+        System.out.println(user.getEmail());
+        System.out.println(user.getName());
+        System.out.println(user.getPassword());
     }
 }
--- src/sns/entity/User.java
+++ src/sns/entity/User.java
@@ -36,7 +36,5 @@ public class User {
     }
     public void setPassword(String password) {
         this.password = password;
-    }
-    
-    
+    }  
 }
