b36ca5ae3fc42c1677cdfa02e1c259b723c3200d
{marimo_7} <{220098tt@yse-c.net}>
2023-05-26 11:16:33 +0900

    update

--- src/Shop/IteratorApp.java
+++ src/Shop/IteratorApp.java
@@ -1,6 +1,7 @@
 package shop;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 public class IteratorApp {
     public static void main(String[] args) {
@@ -14,5 +15,13 @@ public class IteratorApp {
         drinkList.add(drink2);
         drinkList.add(drink3);
 
+        // イテレータに変換
+        Iterator<Drink> iterator = drinkList.iterator();
+        // 次のデータがあれば、ずっと繰り返す
+        while (iterator.hasNext()) {
+            // 次のデータを取得
+            Drink drink = iterator.next();
+            System.out.println(drink.name);
+        }
     }
 }
