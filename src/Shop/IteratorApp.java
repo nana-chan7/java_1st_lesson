e2eb3277be85414621b8e1aae9997604c0ab1ed4
{marimo_7} <{220098tt@yse-c.net}>
2023-05-26 11:38:49 +0900

    iterator

--- src/Shop/IteratorApp.java
+++ src/Shop/IteratorApp.java
@@ -2,6 +2,7 @@ package shop;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.ListIterator;
 
 public class IteratorApp {
     public static void main(String[] args) {
@@ -15,12 +16,29 @@ public class IteratorApp {
         drinkList.add(drink2);
         drinkList.add(drink3);
 
+        System.out.println("-----イテレータ-----");
         // イテレータに変換
         Iterator<Drink> iterator = drinkList.iterator();
         // 次のデータがあれば、ずっと繰り返す
         while (iterator.hasNext()) {
             // 次のデータを取得
             Drink drink = iterator.next();
+            System.out.println(drink.name);
+        }
+
+        System.out.println("-----リストイテレータ-----");
+        // リストイテレータ
+        ListIterator<Drink> listIterator = drinkList.listIterator();
+
+        while (listIterator.hasNext()) {
+            Drink drink = listIterator.next();
+            System.out.println(drink.name);
+        }
+
+        System.out.println(">>>>>");
+
+        while (listIterator.hasPrevious()){
+            Drink drink = listIterator.previous();
             System.out.println(drink.name);
         }
     }
