d72d0ab5b01fedaaf9d42cb60259237f2f41bc30
{marimo_7} <{220098tt@yse-c.net}>
2023-05-26 10:21:42 +0900

    update

--- src/Shop/MapApp.java
+++ src/Shop/MapApp.java
@@ -4,7 +4,6 @@ package shop;
 import java.util.HashMap;
 import java.util.Map;
 
-
 public class MapApp {
     public static void main(String[] args) {
         // Ctrl + . : クイックフィックス
@@ -13,12 +12,34 @@ public class MapApp {
         drinkMap.put("D0002", "紅茶");
         drinkMap.put("D0003", "ほうじ茶");
 
+        System.out.println("-----データ取得-----");
         // データ取得
         String drinkName = drinkMap.get("D0003");
         System.out.println(drinkName);
 
+        System.out.println("-----データ個数-----");
         // データ個数
         int drinkCount = drinkMap.size();
         System.out.println(drinkCount);
+
+        System.out.println("-----キーの繰り返し-----");
+        // キーの繰り返し
+        for (String key : drinkMap.keySet()){
+            System.out.println(key);
+        }
+
+        System.out.println("-----値の繰り返し-----");
+        // 値の繰り返し
+        for (String value : drinkMap.values()) {
+            System.out.println(value);            
+        }
+
+        System.out.println("-----繰り返し-----");
+        // 繰り返し
+        for (String key : drinkMap.keySet()) {
+            String name = drinkMap.get(key);
+            System.out.println(key + ":" + name);            
+        }
+        
     }
 }
