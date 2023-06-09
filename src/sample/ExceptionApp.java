d15e9ede8572587b43eb38a7554e11bd1022fb7e
{marimo_7} <{220098tt@yse-c.net}>
2023-06-09 12:04:32 +0900

    0の除算

---
+++ src/sample/ExceptionApp.java
@@ -0,0 +1,26 @@
+// 2023-06-09 week9
+package sample;
+
+public class ExceptionApp {
+    public static void main(String[] args) {
+        float average;
+
+        average = calculateAverage(300, 110);
+        System.out.println(average);
+
+        average = calculateAverage(300, 0);
+        System.out.println(average);
+    }
+
+    public static Float calculateAverage(int score, int count) {
+        // return (float) score / (float)count;
+
+        if (count == 0){
+            System.out.println("count が 0 です");
+            return 0.f;
+        }
+        int average = score / count;
+        return (float) average;
+
+    }
+}
