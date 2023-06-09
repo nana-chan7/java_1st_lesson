aa480e0ba98c6c02a2b073373f531bf9c58ee59c
{marimo_7} <{220098tt@yse-c.net}>
2023-06-09 13:41:52 +0900

    FileLoadAppクラスでsample.txtのテキストを１行ずつ受け取って表示

--- src/sample/FileLoadApp.java
+++ src/sample/FileLoadApp.java
@@ -1,7 +1,43 @@
+// 2023-06-09 week9
 package sample;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FileLoadApp {
     public static void main(String[] args) {
-        
+        String path = "data/sample.txt";
+        // filein → FileInputStream file = new と入力 →
+        // new → FileInputStream(String name) を選択
+        // ↑ そのままだと波線つくので クイックフィックス: Surround with try/catch を選択
+        try {
+            FileInputStream file = new FileInputStream(path);
+
+            // input → InputStreamReader → new → InputStreamReader(in)
+            InputStreamReader reader = new InputStreamReader(file, "UTF-8");
+            System.out.println("ファイルを開きました。");
+
+            // new → BufferedReader(in)
+            BufferedReader buffer = new BufferedReader(reader);
+
+            // クイックフィックス → Add catch clause to surriuding try
+            // buffer.readLine();
+
+            String line;
+            // ファイルの行に何か入ってる場合 (null でなければ)
+            // １行ずつ受け取る
+            while ((line = buffer.readLine()) != null){
+                System.out.println(line);
+            }
+            
+        } catch (FileNotFoundException e) {
+            System.out.println("ファイルが開けませんでした。");
+        } catch (IOException e) {
+            System.out.println("ファイル読み込みエラー");
+        }
+
     }
 }
