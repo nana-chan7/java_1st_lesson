a3be8d83176d01f40f9a95b2fa8a483334f494d1
{marimo_7} <{220098tt@yse-c.net}>
2023-06-09 11:54:33 +0900

    exception

---
+++ src/sample/ExpceptionApp.java
@@ -0,0 +1,12 @@
+// 2023-06-09 week9
+package sample;
+
+public class ExpceptionApp {
+    public static void main(String[] args) {
+        
+    }
+
+    public static Float calculateAverage(int score, int count) {
+        return (float) score / count;
+    }
+}
