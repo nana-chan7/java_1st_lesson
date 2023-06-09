23a900e8ef94cfaf8ed1af86137048758adea89e
{marimo_7} <{220098tt@yse-c.net}>
2023-06-09 14:29:25 +0900

    ItemListApp の作成

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -6,4 +6,12 @@ import java.util.ArrayList;
 public class ItemList {
     
     ArrayList<Item> list = new ArrayList<>();
+
+    public ItemList() {  // 空のコンストラクタを作成
+        this.loadCSV();
+    }
+
+    public void loadCSV(){
+        System.out.println("CSV 読み込み");
+    }
 }
