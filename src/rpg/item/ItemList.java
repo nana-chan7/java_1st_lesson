67a381d46a14a91c688752aa571217494d68897f
{marimo_7} <{220098tt@yse-c.net}>
2023-06-16 09:22:37 +0900

    sqlファイルの追加

--- src/rpg/item/ItemList.java
+++ src/rpg/item/ItemList.java
@@ -1,10 +1,13 @@
 // 2023-06-09 week9
+// 2023-06-15 2.week1
+
 package rpg.item;
 
 import java.util.ArrayList;
 
 public class ItemList {
-    
+
+    private final String CSV_PATH = "data/item.csv";  // csvファイルを読み込む
     ArrayList<Item> list = new ArrayList<>();
 
     public ItemList() {  // 空のコンストラクタを作成
