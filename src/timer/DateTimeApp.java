fbab0873f177100c9ce51a7d725cd289b725b6b4
{marimo_7} <{220098tt@yse-c.net}>
2023-07-07 12:23:03 +0900

    calender

--- src/timer/DateTimeApp.java
+++ src/timer/DateTimeApp.java
@@ -1,8 +1,22 @@
 // 2023-07-07 2.week4
 package timer;
 
+// import java.sql.Date;
+import java.util.Calendar;
+import java.util.Date;
+
 public class DateTimeApp {
     public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(Calendar.YEAR, 2023);
+        calendar.set(Calendar.MONTH, 6);
+        calendar.set(Calendar.DATE, 7);
+        calendar.set(Calendar.HOUR_OF_DAY, 13);
+        calendar.set(Calendar.MINUTE, 50);
+        calendar.set(Calendar.SECOND, 0);
+
+        Date scheculeTime = calendar.getTime();
+        System.out.println(scheculeTime);
         
     }
 }
