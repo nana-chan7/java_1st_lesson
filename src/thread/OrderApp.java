bdb9ce7d903111dab409be6d7d5fb9cdda079aa3
{marimo_7} <{220098tt@yse-c.net}>
2023-07-07 10:11:48 +0900

    up

--- src/thread/OrderApp.java
+++ src/thread/OrderApp.java
@@ -1,7 +1,18 @@
+// 2023-07-07 2.week4
 package thread;
+
+import java.util.ArrayList;
 
 public class OrderApp {
     public static void main(String[] args) {
-        
+        ArrayList<Item> items = new ArrayList<>();
+        items.add( new Item("コーヒー"));
+        items.add( new Item("紅茶"));
+        items.add( new Item("ほうじ茶"));
+
+        for (Item item : items) {
+            ShopThread thread = new ShopThread(item);
+            thread.start();
+        }
     }
 }
--- src/thread/ShopThread.java
+++ src/thread/ShopThread.java
@@ -1,5 +1,17 @@
+// 2023-07-07 2.week4
 package thread;
 
-public class ShopThread {
-    
+public class ShopThread extends Thread{
+
+    private Item item;
+
+    // Constructor
+    ShopThread(Item item){
+        this.item = item;
+    }
+
+    @Override
+    public void run(){
+        item.order();
+    }
 }
