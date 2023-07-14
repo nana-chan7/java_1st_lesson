ae16eaa1c626760cb8b0d0497329a2204af83bd0
{marimo_7} <{220098tt@yse-c.net}>
2023-07-14 09:16:29 +0900

    exam

---
+++ src/exam47/Animal.java
@@ -0,0 +1,7 @@
+// 2023-07-14 2.week5
+
+package exam47;
+
+public class Animal {
+    public String name;
+}
---
+++ src/exam47/AnimalApp.java
@@ -0,0 +1,10 @@
+// 2023-07-14 2.week5
+
+package exam47;
+
+public class AnimalApp {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        Animal animal = new Animal();
+    }
+}
