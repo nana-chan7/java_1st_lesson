3e6af59521ef0a8f5a046af8805b1fcc4ed65754
{marimo_7} <{220098tt@yse-c.net}>
2023-07-14 11:35:33 +0900

    up

--- src/exam47/q5/Q25.java
+++ src/exam47/q5/Q25.java
@@ -3,9 +3,9 @@ package exam47.q5;
 public class Q25 {
     public static void main(String[] args) {
         int[][] t = {{1,2}, {3, 4}, {5}};
-        for (int[]a : t){
+        for (int[]a : t){ // forearch
             for (int n : a){
-                System.out.println(n + ", ");
+                System.out.print(n + ", ");
             }
         }
     }
