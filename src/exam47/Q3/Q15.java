f093e73f0200b61ed276d959357b0af46faa2252
{marimo_7} <{220098tt@yse-c.net}>
2023-07-14 10:52:49 +0900

    up

--- src/exam47/Q3/Q15.java
+++ src/exam47/Q3/Q15.java
@@ -2,7 +2,7 @@ package exam47.Q3;
 
 public class Q15 {
     enum Attendance {
-        ATTEND, ABSENCE, LEAVE
+        ATTEND, ABSENCE, LEAVE, LATE
     }
     
     public static void main(String[] args) {
--- src/exam47/Q3/Q17.java
+++ src/exam47/Q3/Q17.java
@@ -3,4 +3,6 @@ package exam47.Q3;
 public class Q17 {
     //member 変数
     private int x = 123;
+    // ↑ スーパークラスでも privateなのでアクセスできない 
+    // publicならアクセスできる
 }
